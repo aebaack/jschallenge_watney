023b4a883ff1d812a54ec498a47536e35afd04b3
Aidan Baack <aebaack@gmail.com>
2017-01-12 13:20:22 -0700

    Add encode functionality

--- watneyDecoder.js
+++ watneyDecoder.js
@@ -1,9 +1,15 @@
+'use strict';
+
 function decode(str){
 
 }
 
 function encode(str){
- 
+  let message = [];
+  for (let i = 0; i < str.length; i++) {
+    message.push(str.charCodeAt(i).toString(16));
+  }
+  return message.join(',');
 }
 
 function encodeArc(str) {
