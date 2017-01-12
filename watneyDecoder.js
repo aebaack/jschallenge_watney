371c1298236dbf3a7988a701ee677e8f46f5ec99
Aidan Baack <aebaack@gmail.com>
2017-01-12 13:59:31 -0700

    Passing tests

--- watneyDecoder.js
+++ watneyDecoder.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function decode(str){
-
+  return str.split(',').reduce((message, char) => {
+    const decodedChar = String.fromCharCode(parseInt(char, 16));
+    return message + decodedChar;
+  }, '');
 }
 
 function encode(str){
@@ -13,7 +16,19 @@ function encode(str){
 }
 
 function encodeArc(str) {
-
+  const encodedStr = encode(str).replace(/,/g, '');
+  // 0 - 0
+  // 1 - 22.5
+  // 2 - 45
+  // 4 - 90
+  const arcAngles = [];
+  for (let char of encodedStr) {
+    if (!Number.isInteger(parseInt(char))) {
+      char = parseInt(char, 16);
+    }
+    arcAngles.push(char * 22.5);
+  }
+  return arcAngles.join(',');
 }
 
-module.exports = {decode,encode,encodeArc}
+module.exports = {decode,encode,encodeArc};
